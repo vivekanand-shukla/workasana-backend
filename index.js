4c2367a5280071648a3bf85339e305892235f2a0
VIVEK SHUKLA <primevivek14@gmail.com>
2026-01-17 13:53:32 +0530

    deployment changes

--- index.js
+++ index.js
@@ -27,7 +27,8 @@ const corsOptions = {
     "http://localhost:3000"                        // Optional (Next.js etc)
   ],
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE"],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
   optionsSuccessStatus: 200
 };
 
